d9b5935693db2a754c2e9b3c103f52410b2099b2
kodrat__ <kodratcoc@gmail.com>
2024-06-21 09:31:30 +0700

    first commit

---
+++ app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Providers;
+
+use App\Actions\Fortify\CreateNewUser;
+use App\Actions\Fortify\ResetUserPassword;
+use App\Actions\Fortify\UpdateUserPassword;
+use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Laravel\Fortify\Fortify;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Fortify::createUsersUsing(CreateNewUser::class);
+        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        RateLimiter::for('login', function (Request $request) {
+            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
+
+            return Limit::perMinute(5)->by($throttleKey);
+        });
+
+        RateLimiter::for('two-factor', function (Request $request) {
+            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
+        // Fortify::authenticateUsing(function (Request $request) {
+        //     Validator::make($request->all(), [
+        //         'g-recaptcha-response' => ['required', function($attribute, $value, $fail) {
+        //             $response = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+        //                 'secret' => env('RECAPTCHA_SECRET_KEY'),
+        //                 'response' => $value,
+        //             ]);
+
+        //             if (!$response->json('success')) {
+        //                 $fail('The ' . $attribute . ' is incorrect.');
+        //             }
+        //         }],
+        //     ])->validate();
+
+        //     $user = User::where('email', $request->email)->first();
+
+        //     if ($user && Hash::check($request->password, $user->password)) {
+        //         return $user;
+        //     }
+        // });
+    }
+}
