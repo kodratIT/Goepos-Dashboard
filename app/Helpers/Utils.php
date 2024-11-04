581c63a58a8125d2a362d0c75d60ad8d6f5c7249
kodrat__ <kodratcoc@gmail.com>
2024-11-04 19:25:42 +0700

    bug tanggal

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -68,7 +68,7 @@ function formatTanggal($dateString = '-')
 {
     // Pastikan input berupa format yang valid, lalu konversi menggunakan Carbon
     try {
-        $date = \Carbon\Carbon::parse($dateString); // Parsing tanggal
+        $date = Carbon::parse($dateString, 'Asia/Jakarta'); // Parsing tanggal dengan zona waktu GMT+7
         return $date->format('Y-m-d H:i'); // Format menjadi '2024-10-30 09:19'
     } catch (\Exception $e) {
         // Jika parsing gagal, kembalikan nilai default '-'
